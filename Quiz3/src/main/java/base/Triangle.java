58864019ff694048f653044dcfb60dd580545378
Elizabeth <Elizabeth@wifi-roaming-128-4-101-232.host.udel.edu>
2016-10-10 13:22:16 -0400

    Added check in constructor.

--- Quiz3/src/main/java/base/Triangle.java
+++ Quiz3/src/main/java/base/Triangle.java
@@ -4,17 +4,23 @@ public class Triangle extends GeometricObject {
 	private double side1 = 1.0;
 	private double side2 = 1.0;
 	private double side3 = 1.0;
-	
-	 /** Construct a default triangle, extension of geometric object */
-	public Triangle() {}
-	
-	 /** Construct a triangle, extension of geometric object, with 3 args: doubles that will be the 
-	  * values of the sides of the triangle created. */
 
-	public Triangle(double side1, double side2, double side3) {
+	/** Construct a default triangle, extension of geometric object */
+	public Triangle() {
+	}
+
+	/**
+	 * Construct a triangle, extension of geometric object, with 3 args: doubles
+	 * that will be the values of the sides of the triangle created.
+	 * 
+	 * @throws TriangleException
+	 */
+
+	public Triangle(double side1, double side2, double side3) throws TriangleException {
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
+		this.getArea();
 	}
 
 	/** Returns value of side1 attribute */
@@ -22,7 +28,7 @@ public class Triangle extends GeometricObject {
 		return side1;
 	}
 
-	/** Sets value of side1 attribute*/
+	/** Sets value of side1 attribute */
 	public void setSide1(double side1) {
 		this.side1 = side1;
 	}
@@ -32,7 +38,7 @@ public class Triangle extends GeometricObject {
 		return side2;
 	}
 
-	/** Sets value of side1 attribute*/
+	/** Sets value of side1 attribute */
 	public void setSide2(double side2) {
 		this.side2 = side2;
 	}
@@ -42,51 +48,63 @@ public class Triangle extends GeometricObject {
 		return side3;
 	}
 
-	/** Sets value of side1 attribute*/
+	/** Sets value of side1 attribute */
 	public void setSide3(double side3) {
 		this.side3 = side3;
 	}
-	
-	/** Returns value of the triangle's perimeter if the triangle is real.
-	 * @throws TriangleException*/
-	public double getPerimeter() throws TriangleException{
-		double p = ((side1 + side2 + side3)/2);	
-		double num = (p*(p-side1)*(p-side2)*(p-side3));
+
+	/**
+	 * Returns value of the triangle's perimeter if the triangle is real.
+	 * 
+	 * @throws TriangleException
+	 */
+	public double getPerimeter() throws TriangleException {
+		double p = ((side1 + side2 + side3) / 2);
+		double num = (p * (p - side1) * (p - side2) * (p - side3));
 		if (num <= 0)
 			throw new TriangleException(this, "That triangle isn't real!");
 		else
 			return side1 + side2 + side3;
 	}
-	
-	/** Returns value of the triangle's area if the triangle is real. 
-	 * @throws TriangleException*/
+
+	/**
+	 * Returns value of the triangle's area if the triangle is real.
+	 * 
+	 * @throws TriangleException
+	 */
 	@Override
 	public double getArea() throws TriangleException {
-		double p = (this.getPerimeter()/2);	
-		double num = (p*(p-side1)*(p-side2)*(p-side3));
+		double p = (this.getPerimeter() / 2);
+		double num = (p * (p - side1) * (p - side2) * (p - side3));
 		if (num <= 0)
 			throw new TriangleException(this, "That triangle isn't real!");
 		else
 			return Math.sqrt(num);
 	}
-	
-	/** Returns information about the triangle: the lengths of the sides, the color, and whether or not it is filled. */
-	public String toString(){
+
+	/**
+	 * Returns information about the triangle: the lengths of the sides, the
+	 * color, area, perimeter, and whether or not it is filled.
+	 */
+	public String toString() {
 		if (this.isFilled())
 			try {
-				return("The length of the sides are " +  side1 +", "+ side2+", and "+ side3 + ". The color is " + getColor() + ". It is filled. The area is " + getArea() + ", the perimeter is " + getPerimeter());
+				return ("The length of the sides are " + side1 + ", " + side2 + ", and " + side3 + ". The color is "
+						+ getColor() + ". It is filled. The area is " + getArea() + ", the perimeter is "
+						+ getPerimeter());
 			} catch (TriangleException e) {
 				e.printStackTrace();
-				return("Your triangle isn't real!");
+				return ("Your triangle isn't real!");
 			}
-		else 
+		else
 			try {
-				return("The length of the sides are " +  side1 +", "+ side2+", and "+ side3 + ". The color is " + getColor() + ". It is not filled. The area is " + getArea() + ", the perimeter is " + getPerimeter());
+				return ("The length of the sides are " + side1 + ", " + side2 + ", and " + side3 + ". The color is "
+						+ getColor() + ". It is not filled. The area is " + getArea() + ", the perimeter is "
+						+ getPerimeter());
 			} catch (TriangleException e) {
 				e.printStackTrace();
-				return("Your triangle isn't real!");
+				return ("Your triangle isn't real!");
 			}
 	}
-
 
 }
--- Quiz3/src/test/java/test/Triangle_Test.java
+++ Quiz3/src/test/java/test/Triangle_Test.java
@@ -1,4 +1,5 @@
 package test;
+
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -7,55 +8,90 @@ import base.TriangleException;
 
 public class Triangle_Test {
 	@Test
-	public void RealTriangleArea(){
-		Triangle tri = 	new Triangle(3, 4, 5);
+	public void RealTriangleArea() {
+		Triangle tri = new Triangle();
+		try {
+			tri = new Triangle(3, 4, 5);
+		} catch (TriangleException e1) {
+			e1.printStackTrace();
+			System.out.println("Oh?? Wasn't expecting catch.");
+		}
 		try {
 			tri.getArea();
 			assertTrue(tri.getArea() == 6);
 		} catch (TriangleException e) {
-			System.out.println("Stop that!");
+			System.out.println("Oh?? Wasn't expecting catch.");
 			e.printStackTrace();
 		}
 	}
-		
-		@Test
-	public void RealTrianglePeri(){
-		Triangle tri = 	new Triangle(3, 4, 5);
+
+	@Test
+	public void RealTrianglePeri() {
+		Triangle tri = new Triangle();
+
+		try {
+			tri = new Triangle(3, 4, 5);
+		} catch (TriangleException e1) {
+			System.out.println("Oh?? Wasn't expecting catch.");
+			e1.printStackTrace();
+		}
 		try {
 			tri.getPerimeter();
 			assertTrue(tri.getPerimeter() == 12);
 		} catch (TriangleException e) {
-			System.out.println("Gross!");
+			System.out.println("Oh?? Wasn't expecting catch.");
 			e.printStackTrace();
 		}
 	}
-	
-	@Test(expected = TriangleException.class) 
-	public void ImaginaryTriangleArea(){
 
-		Triangle tri = 	new Triangle(2, 8, 2);
+	@Test(expected = TriangleException.class)
+	public void ImaginaryTriangleConstructor() {
+		try {
+			Triangle tri = new Triangle(2, 8, 2);
+		} catch (TriangleException e1) {
+			System.out.println("Caught it! Constructor.");
+			e1.printStackTrace();
+		}
+		fail("Didn't catch the exception!?");
+
+	}
+
+	@Test(expected = TriangleException.class)
+	public void ImaginaryTriangleArea() {
+		Triangle tri = new Triangle();
+		try {
+			 tri = new Triangle(2, 3, 2);
+		} catch (TriangleException e1) {
+			System.out.println("Oh?? Wasn't expecting catch.");
+			e1.printStackTrace();
+		}
+		tri.setSide1(80);
 		try {
 			tri.getArea();
 		} catch (TriangleException e) {
-			System.out.println("Stop that!");
+			System.out.println("Caught it! Area.");
 			e.printStackTrace();
 		}
 		fail("Didn't catch the exception!?");
 	}
 
-	@Test(expected = TriangleException.class) 
-	public void ImaginaryTrianglePeri(){
+	@Test(expected = TriangleException.class)
+	public void ImaginaryTrianglePeri() {
 
-		Triangle tri = 	new Triangle(2, 8, 2);
-
+		Triangle tri = new Triangle();
+		try {
+			tri = new Triangle(2, 3, 2);
+		} catch (TriangleException e1) {
+			System.out.println("Oh?? Wasn't expecting catch.");
+			e1.printStackTrace();
+		}
+		tri.setSide1(80);
 		try {
 			tri.getPerimeter();
 		} catch (TriangleException e) {
-			System.out.println("Gross!");
+			System.out.println("Caught it! Perimeter.");
 			e.printStackTrace();
 		}
 		fail("Didn't catch the exception!?");
-	}	
+	}
 }
-
-
